32343b17f970777925698d54ed17191755c0df51
Gabriel <contato.magalhaesgabriel@gmail.com>
2025-03-13 22:24:50 -0300

    update

--- etl/extract.py
+++ etl/extract.py
@@ -4,7 +4,33 @@ from datetime import datetime
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base, BitcoinPreco
+import os
 
+
+
+
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+
+
+DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+
+def create_database():
+    Base.metadata.create_all(engine)
 
 def extract_dados_bitcoin():
     url = 'https://api.coinbase.com/v2/prices/spot'
@@ -32,13 +58,27 @@ def transform_dados_bitcoin(data):
 
 
 def load_dados_pgsql(data):
-    pass
-    
+    session = Session()
+    try:
+        bitcoin_preco = BitcoinPreco(**data)
+        session.add(bitcoin_preco)
+        session.commit()
+    except Exception as e:
+        print(f"Erro ao inserir dados no banco de dados: {e}")
+        session.rollback()
+    finally:
+        session.close()
 
+    
+def pipeline():
+    dados_json = extract_dados_bitcoin()
+    dados_transformados = transform_dados_bitcoin(dados_json)
+    load_dados_pgsql(dados_transformados)
 
 if __name__ == '__main__':
+    create_database()
+    print('Banco de dados criado com sucesso')
     while True:
-        dados = extract_dados_bitcoin()
-        dados_transformados = transform_dados_bitcoin(dados)
-        print(dados_transformados)
-        time.sleep(5)
+        pipeline()
+        print('Dados inseridos com sucesso')
+        time.sleep(15)
